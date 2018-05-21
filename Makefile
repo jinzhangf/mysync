dd873e35464d622d27265a23b3d30f5ae61451d2
dylanfang <dylanfang@tencent.com>
2018-05-21 15:10:33 +0800

    【lx.cpp】 windows路径改成linux路径

--- Makefile
+++ Makefile
@@ -1,2 +1,3 @@
 all:
 	g++ mysync.cpp -o mysync
+	g++ lx.cpp -o lx
---
+++ lx.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <sstream>
+#include <map>
+#include <set>
+#include <string>
+#include <cstdlib>
+#include <cctype>
+#include <windows.h>
+#include <sys/stat.h>
+
+using namespace std;
+
+string exec(const char* cmd) 
+{
+	FILE* pipe = popen(cmd, "r");
+	if (!pipe) return cmd + string (" ERROR");
+	static char buffer[4096];
+	string result = "";
+	
+	while(!feof(pipe)) {
+		if(fgets(buffer, 4096, pipe) != NULL) {
+			for (size_t i = 0; i < 4096; ++i)
+			{
+				if (buffer[i] == '\0')
+					break;
+				
+				if (buffer[i] == '\\')
+					buffer[i] = '/';
+			}
+			result += buffer;
+		}
+	}
+	
+	pclose(pipe);
+	return result;
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2) {
+		cerr << "WRONG argc = [" << argc << "]" << endl;
+		return -1;
+	}
+	
+	string cmd;
+	for (size_t i = 1; i < argc; ++i)
+	{
+		cmd.append(argv[i]).append(" ");
+	}
+	cout << exec(cmd.c_str()) << flush;
+	
+	return 0;
+}
